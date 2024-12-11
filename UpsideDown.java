9d44edfc1dbf6c4bc949175f5ffb551918642430
Nirmalraj3103 <rajnirmal2359@gmail.com>
2024-12-11 07:58:28 +0530

    Add files via upload

---
+++ UpsideDown.java
@@ -0,0 +1,25 @@
+package javaTask;
+
+import java.util.Scanner;
+
+public class UpsideDown {
+	
+	public static void main(String[] args) {
+		int i,j;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of Rows");
+		int n = sc.nextInt();
+		
+		for (i=1;i<=n;i++) {
+			
+			for (j=n;j>=i;j--) {
+				System.out.print(i);
+			}
+			
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+
+}
