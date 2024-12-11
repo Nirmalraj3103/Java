a87252c358de338b51f52b1409a54aeb4fced6a7
Nirmalraj3103 <rajnirmal2359@gmail.com>
2024-12-11 08:01:48 +0530

    Add files via upload

---
+++ UpsideDownReversed.java
@@ -0,0 +1,26 @@
+package javaTask;
+
+import java.util.Scanner;
+
+public class UpsideDownReversed {
+	
+public static void main(String[] args) {
+		
+		int i, j;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of Rows : ");
+		int n=sc.nextInt();
+		
+		for(i=n;i>=1;i--) {
+			
+			for (j=n;j>=n-i+1;j--) {
+				
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
