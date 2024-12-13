5fae4b28bf7c2c5e0c5b06155987a54049612af6
Nirmalraj3103 <rajnirmal2359@gmail.com>
2024-12-13 18:28:18 +0530

    Add files via upload

---
+++ RightMirrorTriangle.java
@@ -0,0 +1,19 @@
+package javaTask;
+
+public class RightMirrorTriangle {
+	
+	public static void main(String[] args) {
+        int rows = 6;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) 
+                System.out.print(" ");    
+            
+            for (int j = 1; j <= i; j++) 
+                System.out.print("*");
+            
+            System.out.println(); 
+        }
+    }
+
+}
